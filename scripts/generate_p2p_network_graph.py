0bfae590f3d540b82379b88fcf852c7ded7d6650
Irshad Khan <khanirshad2019@gmail.com>
2025-02-07 01:08:50 +0530

    generate blockchain graph script

--- scripts/generate_p2p_network_graph.py
+++ scripts/generate_p2p_network_graph.py
@@ -13,13 +13,18 @@ def load_network_graph_from_file(filepath):
         for line in f:
             node_data = list(map(int, line.strip().split()))
             source_node = node_data[0]
-            for target_node in node_data[1:]:
-                graph.add_edge(source_node, target_node)
+            target_node = node_data[1]
+            graph.add_edge(source_node, target_node)
     return graph
 
 def plot_network_graph(graph, directory):
-    layout = nx.fruchterman_reingold_layout(graph)
-    nx.draw(graph, layout,edge_color='black',node_color='lightgreen',node_size=1500,with_labels=True,)
+    layout = nx.spring_layout(graph)
+    num_nodes = len(graph.nodes)  
+    # Scale size based on nodes
+    size = max(10, num_nodes * 1.05)  
+    # print(size)
+    plt.figure(figsize=(size, size))
+    nx.draw(graph, layout,edge_color='black',node_color='lightgreen',node_size=2000,with_labels=True,)
     
     # create targte directory if not exists
     os.makedirs(directory, exist_ok=True)
